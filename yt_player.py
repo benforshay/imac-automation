5797b9d74037399572144e927d29beb2cfb6a0c9
benforshay <benforshay@gmail.com>
2026-02-15 07:21:58 -0500

    Updated player with stop logic from public folder

--- yt_player.py
+++ yt_player.py
@@ -1,3 +1,18 @@
 import subprocess
+import time
+import sys
 
-subprocess.Popen(["/usr/local/bin/mpv", "--no-video", "--no-terminal", "https://www.youtube.com/watch?v=v2NjCGVxA8I"])
+# Starts the music
+url = "https://www.youtube.com/watch?v=v2NjCGVxA8I"
+player = subprocess.Popen(["mpv", "--no-video", url])
+
+print("RUNNING. Press Ctrl+C or Close Terminal to stop music.")
+
+try:
+    while True:
+        time.sleep(1)
+except KeyboardInterrupt:
+    player.terminate()
+    sys.exit()
+finally:
+    player.kill() # The final 'Shut Up' command
